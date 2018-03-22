b45e85e3a83622108d42e37aa2d6814b07bdb8df
LikeJust <diddydwns135@gmail.com>
2018-03-23 04:14:19 +0900

    2908

---
+++ 2908.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1;
+    cin >> str2;
+
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+
+    if (str1 > str2) {
+        cout << str1;
+    } else {
+        cout << str2;
+    }
+}
